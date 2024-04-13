d17dfe786bcbf5c32835291811c8fc126b00e028
Sekar Nagarajan <sekarnagarajan@gmail.com>
2024-04-13 22:37:44 +0530

    js

--- E-Com/backEnd/controllers/orderController.js
+++ E-Com/backEnd/controllers/orderController.js
@@ -1,17 +1,21 @@
 const orderModel = require("../models/orderModel");
 const productModel = require("../models/productModel");
+//Create Order - /api/v1/order
 
-exports.createOrder = (req, res, next) => {
+exports.createOrder = async (req, res, next) => {
   //console.log(req.body, "DATA");
 
   const cartItems = req.body;
   const amount = Number(
     cartItems.reduce((acc, item) => acc + item.product.price * item.qty, 0)
   ).toFixed(2);
-  console.log(amount, "amount");
+  const status = "pending";
+
+  const order = await orderModel.create({ cartItems, amount, status });
+  //console.log(amount, "amount");
 
   res.json({
     success: true,
-    message: "Order works!",
+    order,
   });
 };
